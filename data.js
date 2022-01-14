a329a1a2b4d4791924eb4c234fdcd9ff162aed19
Lindsey Laveirge <llaveirge@gmail.com>
2022-01-14 16:01:52 -0700

    Add and style table, and local storage save

--- data.js
+++ data.js
@@ -9,3 +9,16 @@ var data = {
   saturday: [],
   id: 1
 };
+
+var previousEntriesJSON = localStorage.getItem('data-local-storage');
+if (previousEntriesJSON !== null) {
+  data = JSON.parse(previousEntriesJSON);
+}
+
+function beforeunloadHandler(event) {
+
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('data-local-storage', dataJSON);
+}
+
+window.addEventListener('beforeunload', beforeunloadHandler);
