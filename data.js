3d1ba8270f77a0b9810fa6933cc27ceeb8363c46
Michael Tang <tngmichael28@gmail.com>
2022-01-14 14:08:00 -0800

    Made data model for entries.

---
+++ data.js
@@ -0,0 +1,11 @@
+/* exported data */
+var data = {
+  sunday: [],
+  monday: [],
+  tuesday: [],
+  wednesday: [],
+  thursday: [],
+  friday: [],
+  saturday: [],
+  id: 1
+};
--- main.js
+++ main.js
@@ -1,13 +1,44 @@
+/* global data */
 var $addButton = document.getElementById('add-button');
 var $modal = document.getElementById('entry-modal');
-// var $form = document.getElementById('entry-form');
-$addButton.addEventListener('click', goToAdd);
+var $form = document.getElementById('entry-form');
+var $timeSelection = document.getElementById('time-selection');
 
-function goToAdd() {
+function goToAdd(event) {
   $modal.style.display = 'block';
 }
 
-//  function renderTime() {
-//   var $timeOption = document.createElement('option');
-//   $timeOption.setAttribute('value', timeIncrement);
-// }
+function renderTime() {
+  var timeIncrement = '';
+  for (var i = 0; i < 24; i++) {
+    var $timeOption = document.createElement('option');
+    timeIncrement = i + ':00';
+    $timeOption.setAttribute('value', timeIncrement);
+    $timeOption.textContent = timeIncrement;
+    $timeSelection.appendChild($timeOption);
+  }
+}
+
+function saveEntry(event) {
+  event.preventDefault();
+  var formData = new FormData($form);
+  var dayOfWeek = formData.get('day-selection');
+  var timeOfDay = formData.get('time-selection');
+  var description = formData.get('description');
+  var entry = {
+    dayOfWeek: dayOfWeek,
+    timeOfDay: timeOfDay,
+    description: description,
+    id: data.id
+  };
+  // console.log(dayOfWeek);
+  // console.log(timeOfDay);
+  // console.log(description);
+  data[dayOfWeek].unshift(entry);
+  data.id++;
+  // console.log(data);
+}
+
+$addButton.addEventListener('click', goToAdd);
+renderTime();
+$form.addEventListener('submit', saveEntry);
