37a4f41b5ce624a08778e1437f2ec7ce372c051a
Lindsey Laveirge <llaveirge@gmail.com>
2022-01-14 18:35:11 -0700

    SORT THE TABLE!

--- main.js
+++ main.js
@@ -66,8 +66,51 @@ function updateTable(event) {
     $tableEntries.rows.item(i).cells[0].textContent = data[day][i - 1].timeOfDay;
     $tableEntries.rows.item(i).cells[1].textContent = data[day][i - 1].description;
   }
+
+  sortTable();
 }
+
 $addButton.addEventListener('click', goToAdd);
 renderTime();
 $form.addEventListener('submit', saveEntry);
 $weekButtons.addEventListener('click', updateTable);
+
+// Lindsey's sort:
+function sortTable() {
+  var rows, switching, i, x, y, shouldSwitch;
+
+  switching = true;
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // start by saying: no switching is done:
+    switching = false;
+    rows = $tableEntries.rows;
+    /* Loop through all table rows (except the
+    first, which contains table headers): */
+    for (i = 1; i < (rows.length - 1); i++) {
+      // start by saying there should be no switching:
+      shouldSwitch = false;
+
+      /* Get the two elements you want to compare,
+      one from current row and one from the next: */
+      x = $tableEntries.rows.item(i).cells[0].textContent;
+      y = $tableEntries.rows.item(i + 1).cells[0].textContent.split(':')[0];
+      // check if the two rows should switch place:
+      if (y !== '') {
+        if (+x.split(':')[0] > +y.split(':')[0]) {
+        // if so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      }
+      // end of entry if
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
+}
